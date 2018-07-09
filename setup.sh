a0673934163e5d887456f1d210d62b1683e5ece5
eecourse <741974115@qq.com>
2018-07-10 00:31:50 +0800

    A script for setting up working directory

---
+++ setup.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+source ~/.bashrc
+
+echo "Enter your design name:"
+read design
+
+ori_dir=/qixin/proj_users/swru/verilog_labs/rsh
+ori_design=counter10
+syn_ori=${ori_dir}/syn
+sim_ori=${ori_dir}/sim
+
+
+design_dir=./${design}
+rtl_dir=./${design}/rtl
+tb_dir=./${design}/tb
+sim_dir=./${design}/sim
+syn_dir=./${design}/syn
+lib_dir=./${design}/library
+
+ori_syn_scr=${syn_ori}/syn.tcl
+ori_sdc_scr=${syn_ori}/syn.sdc
+setup_scr=${syn_ori}/.synopsys_dc.setup
+syn_make=${syn_ori}/Makefile
+
+syn_lib=${ori_dir}/library/sc_max.db
+
+sim_time=${ori_dir}/tb/timescale.v
+
+ori_sim_make=${sim_ori}/Makefile
+
+syn_scr=${syn_dir}/syn.tcl
+sdc_scr=${syn_dir}/sdc.tcl
+sim_make=${sim_dir}/Makefile
+
+
+
+dir_list=( $design_dir $rtl_dir $tb_dir $sim_dir $syn_dir $lib_dir )
+
+scr_list=( $ori_syn_scr $ori_sdc_scr $setup_scr $syn_make )
+
+
+if [ ! -d ${design_dir} ]; then
+  echo "${design} working directory is setting up..."
+
+  for dir in ${dir_list[@]}; do
+    mkdir $dir
+  done
+  
+  for scr in ${scr_list[@]}; do
+      \cp -rf $scr $syn_dir
+  done
+  \cp -rf $syn_lib      $lib_dir
+  \cp -rf $ori_sim_make $sim_dir
+  \cp -rf $sim_time     $tb_dir
+
+  touch ${rtl_dir}/${design}.v ${tb_dir}/${design}_tb.v
+
+  sed -i "s/${ori_design}/${design}/g" $sim_make $syn_scr
+
+else 
+  echo "${design} exists, try another one"
+
+fi
+
+
